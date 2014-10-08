469ee2b403b034244a9fac0514a50ebe98b6a90f
Christopher Torgalson <manager@bedlamhotel.com>
2014-10-08 16:47:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Cookies Please
+
+This is a very simple plugin for dealing with the [EU's guidelines on Cookies](http://ico.org.uk/for_organisations/privacy_and_electronic_communications/~/media/documents/library/Privacy_and_electronic/Practical_application/guidance_on_the_new_cookies_regulations.ashx). It does only one job: attempt to set a single cookie with the user's cookie preference. This cookie can be used (in conjunction with jquery.cookie, a dependency of this plugin) by other scripts to find out if they can/should use cookies. It can even be used to determine whether other scripts can be loaded at all.
+
+## Why another cookie plugin?
+
+There are already lots of plugins like this, but the ones I've looked at
+suffer from two common problems:
+
+1. **They often insist on providing their own markup and css**. This is
+   aggravating if you're already working with a front-end framework like
+   [Zurb's Foundation](http://foundation.zurb.com/) or [Twitter's Bootstrap](http://getbootstrap.com/).
+   This one is a jQuery plugin called on a jQuery collection.
+2. **They sometimes have unnecessarily complex ways of integrating with
+   other scripts**. This one sets a cookie you can use to decide how or
+   whether to call other scripts.
+3. **They can be fairly large scripts**. This one is relatively small.
+4. **They sometimes impose a specific choice model**. This one allows
+   you to use 'Yes' and 'No' buttons, or simply a 'Yes' button (i.e.
+   with text like "You need to have cookies enabled for this website to
+   work [ Ok ]"
+
+## Usage
+
+### HTML
+
+    <!-- Using Foundation's alert boxes -->
+    <div data-alert class="alert-box info cookies-please hide">
+      <div class="row">
+        <div class="columns medium-12">
+          This is an info alert with a radius.
+          <button id="i-like-cookies">Go cookies!</button>
+          <button id="i-hate-cookies">F--- cookies!</button>
+        </div>
+      </div>
+    </div>
+
+### Javascript
+
+    $(document).ready(function () {
+      $('.alert-info.cookies-please').cookiesPlease();
+    });
+
---
+++ scripts/jquery.cookiesPlease.js
@@ -0,0 +1,54 @@
+/**
+ * @param object overrides
+ *  This contains configuration options for the plugin:
+ *  -- iLikeCookiesSelector (string): a valid CSS selector uniquely identifying
+ *     the element the user needs to click/tap/etc to agree to accept cookies
+ *  -- iHateCookiesSelector (string): a valid CSS selector uniquely identifying
+ *     the element the user needs to click/tap/etc to refuse to accept cookies
+ *  -- cookieExpiry (int): the cookie we set to store the user's preferences re:
+ *     cookies will expire in this many days
+ *
+ * @note Both the iLikeCookiesSelector and the iHateCookiesSelector items in the
+ *       settings variable are used inside jQuery's .find() function, so the do
+ *       not need to be globlly unique in the document.
+ *
+ * @see http://ico.org.uk/for_organisations/privacy_and_electronic_communications/~/media/documents/library/Privacy_and_electronic/Practical_application/guidance_on_the_new_cookies_regulations.ashx
+ */
+$.fn.cookiesPlease = function (overrides) {
+  "use strict";
+  // Define overridable defaults, and get the current cookie value:
+  var defaults = {
+    iLikeCookiesSelector: '#i-like-cookies',
+    iHateCookiesSelector: '#i-hate-cookies',
+    cookieExpiry: 1
+  },
+  settings = $.extend({}, defaults, overrides),
+  cookieValue = $.cookie('cookiesPlease');
+  // Return collection for chaining:
+  return this.each(function () {
+    // We actually don't need to do anything unless the cookie has no value:
+    if (cookieValue === undefined) {
+      // We only need to make a collection out of the current element once:
+      var $current = $(this);
+      // Define click handlers on the 'like' and 'hate' buttons. Click handlers
+      // are identical except for the cookie value ('yes' or 'no'):
+      $current
+        .find(settings.iLikeCookiesSelector).click(function (e) {
+          // Prevent default in case the current element is a link:
+          e.preventDefault();
+          // Set the cookie value:
+          $.cookie('cookiesPlease', 'yes', {expires: settings.cookieExpiry, path: '/'});
+          // Hide the message/modal etc:
+          $current.hide();
+        })
+        .end()
+        .find(settings.iHateCookiesSelector).click(function (e) {
+          e.preventDefault();
+          $.cookie('cookiesPlease', 'no', {expires: settings.cookieExpiry, path: '/'});
+          $current.hide();
+        })
+        .end()
+        .show();
+    }
+  });
+};
